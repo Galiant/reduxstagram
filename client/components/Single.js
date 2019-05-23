02e94a57c0ab5d79d7484fb9a0a56da693e73053
Antonijo <antonijodev@gmail.com>
2019-05-23 12:03:01 +0100

    Added components

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+        I am a Single Grid
+      </div>
+    )
+  }
+});
+
+export default Single;
